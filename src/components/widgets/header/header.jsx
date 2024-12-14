8c335583d0c2f4f973b6d4cc42e06bc1a251603c
MEDOYED <mmax26072005@gmail.com>
2024-12-14 12:41:28 +0100

    add link to home-page in logo in header

--- src/components/widgets/header/header.jsx
+++ src/components/widgets/header/header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import hithubIcon from "../../../assets/img/header/github-icon.svg";
 
 import "./header.css";
@@ -7,7 +8,9 @@ const Header = () => {
     <>
       <header className="header">
         <div className="container header__content   ">
-          <div className="header__logo">Dictionary of the words</div>
+          <div className="header__logo">
+            <Link to={"/"}>Dictionary of the words</Link>
+          </div>
           <nav className="header__nav">
             <ul className="header__nav-links">
               <li className="header__nav-link">
