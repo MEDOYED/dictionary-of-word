3c5d9b6820261142996152de6e92eb685dc25da3
MEDOYED <mmax26072005@gmail.com>
2025-01-16 10:49:56 +0100

    improved the generation of links to Wikipedia

--- src/components/widgets/word-desc-heading/word-desc-heading.jsx
+++ src/components/widgets/word-desc-heading/word-desc-heading.jsx
@@ -9,16 +9,20 @@ const WordDescHeading = ({ data }) => {
         Meaning of <span className="word-desc__heading--bold">{data[0]?.word}</span> in English
       </h1>
       <ul className="word-desc__links">
-        <li className="word-desc__link">
-          <a href={data[0]?.sourceUrls[0]} target="_blank">
-            <img src={wikiLogo} alt="link to Wikipedia" />
-          </a>
-        </li>
-        <li className="word-desc__link">
-          <a href={data[0]?.sourceUrls[1]} target="_blank">
-            <img src={wikiLogo} alt="link to Wikipedia" />
-          </a>
-        </li>
+        {data[0]?.sourceUrls[0] && (
+          <li className="word-desc__link">
+            <a href={data[0]?.sourceUrls[0]} target="_blank">
+              <img src={wikiLogo} alt="link to Wikipedia" />
+            </a>
+          </li>
+        )}
+        {data[0]?.sourceUrls[1] && (
+          <li className="word-desc__link">
+            <a href={data[0]?.sourceUrls[1]} target="_blank">
+              <img src={wikiLogo} alt="link to Wikipedia" />
+            </a>
+          </li>
+        )}
       </ul>
     </>
   );
